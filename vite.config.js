5c1300abb52b5e60be6fab53f188c660d94285be
Lazar993 <lazartanasko@gmail.com>
2026-02-15 11:11:53 +0000

    production adjustements

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/filament-osm.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/filament-osm.js'
+            ],
             refresh: [
                 ...refreshPaths,
                 'app/Http/Livewire/**',
@@ -12,4 +16,9 @@ export default defineConfig({
             ],
         }),
     ],
-})
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
+});
+
