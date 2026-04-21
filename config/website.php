fbc17f6af3085f33b0a373d71fe950fb8b1c3370
Lazar Tanasković <lazartanasko@gmail.com>
2026-04-21 09:26:44 +0200

    config constants in env

--- config/website.php
+++ config/website.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-	'currency' => 'EUR',
-	'deposit_rate' => 0.3,
-	'contact_email' => 'aparto@gmail.com',
-	'apartments_per_page' => 12,
-	'homepage_apartments_limit' => 16,
-	'homepage_section_apartments_limit' => 8,
-	'homepage_trending_period_days' => 30,
-	'homepage_best_rated_min_reviews' => 3,
+	'currency' => env('CURRENCY', 'EUR'),
+	'deposit_rate' => env('DEPOSIT_RATE', 0.3),
+	'contact_email' => env('CONTACT_EMAIL', 'aparto@gmail.com'),
+	'apartments_per_page' => env('APARTMENTS_PER_PAGE', 12),
+	'homepage_apartments_limit' => env('HOMEPAGE_APARTMENTS_LIMIT', 16),
+	'homepage_section_apartments_limit' => env('HOMEPAGE_SECTION_APARTMENTS_LIMIT', 8),
+	'homepage_trending_period_days' => env('HOMEPAGE_TRENDING_PERIOD_DAYS', 1000),
+	'homepage_best_rated_min_reviews' => env('HOMEPAGE_BEST_RATED_MIN_REVIEWS', 3),
 ];
